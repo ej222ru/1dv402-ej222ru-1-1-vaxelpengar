6ab8ccf34282ac25eab3f9616af25c89d3bf4cba
ej222ru <ej222ru@student.lnu.se>
2014-09-08 20:38:20 +0200

    Framework labb 3

---
+++ 1DV402.S1L03C/SalaryRevision/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryRevision
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+
+
+
+
+		}
+
+
+		private static bool IsContinuing()
+		{
+			Console.WriteLine("");
+			Console.BackgroundColor = ConsoleColor.Blue;
+			Console.WriteLine(rm.GetString("Continue_Prompt"));
+			Console.BackgroundColor = ConsoleColor.Black;
+			ConsoleKeyInfo cki = Console.ReadKey(true);
+			Console.WriteLine("");
+			// or should the screen be cleared???   Console.Clear();
+			return (cki.Key != ConsoleKey.Escape);
+
+		}
+
+		private static int ReadInt(string prompt)
+		{
+		}
+		private static int[] ReadSalaries(int count)
+		{
+		}
+		private static void viewMessage(ConsoleColor backgroundColor = ConsoleColor.Blue, ConsoleColor foregroundColor = ConsoleColor.White)
+		{
+		}
+		private static void ViewResult(string prompt)
+		{
+		}
+		private static int ReadInt(int[] salaries)
+		{
+		}
+
+	}
+
+	class MyExtensions
+	{
+
+		public static int Dispersion(this int[] source)
+		{
+		}
+
+		public static int Median(this int[] source)
+		{
+		}
+		
+	}
+}
