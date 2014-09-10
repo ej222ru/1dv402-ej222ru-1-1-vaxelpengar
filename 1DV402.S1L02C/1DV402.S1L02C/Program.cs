e34cfc8d33ec7faaf320d01105756ee64e3988bf
ej222ru <ej222ru@student.lnu.se>
2014-09-10 07:33:44 +0200

    Removed prfix from lab 2

--- 1DV402.S1L02C/1DV402.S1L02C/Program.cs
+++ 1DV402.S1L02C/1DV402.S1L02C/Program.cs
@@ -10,104 +10,96 @@ namespace _1DV402.S1L02C
 {
 	class drawDiamond	
 	{
+		// Declare a resource manager to retrieve resources in all class methods.
 		static ResourceManager rm;
 		const int MAX_ASTERISK = 79;
 
-		private static void viewMessage(string sMessage, bool bIsError = false)
+		private static void viewMessage(string message, bool isError = false)
 		{
 			Console.WriteLine("");
-			if (bIsError)
+			if (isError)
 				Console.BackgroundColor = ConsoleColor.Red;
 			else
 				Console.BackgroundColor = ConsoleColor.Green;
-			Console.WriteLine(sMessage);
+			Console.WriteLine(message);
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.WriteLine("");
 		}
 
 		static void Main(string[] args)
 		{
+			// Create a resource manager to retrieve resources.
 			rm = new ResourceManager("_1DV402.S1L02C.Strings", Assembly.GetExecutingAssembly());
-			byte byWaist = 0;
+			byte waist = 0;
 			do
 			{
-
-				byWaist = ReadOddByte(rm.GetString("OddNumberAsterisk_Prompt"));
-				RenderDiamond(byWaist);
-
+				waist = ReadOddByte(rm.GetString("OddNumberAsterisk_Prompt"));
+				RenderDiamond(waist);
 
 			} while (IsContinuing());
 		}
 
 		private static bool IsContinuing()
 		{ 
-				Console.WriteLine("");
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.WriteLine(rm.GetString("Continue_Prompt"));
-				Console.BackgroundColor = ConsoleColor.Black; 
-				ConsoleKeyInfo cki = Console.ReadKey(true);
-				Console.WriteLine("");
-				// or should the screen be cleared???   Console.Clear();
-				return (cki.Key != ConsoleKey.Escape);
-
+			Console.WriteLine("");
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.WriteLine(rm.GetString("Continue_Prompt"));
+			Console.BackgroundColor = ConsoleColor.Black; 
+			ConsoleKeyInfo cki = Console.ReadKey(true);
+			Console.WriteLine("");
+			// or should the screen be cleared???   Console.Clear();
+			return (cki.Key != ConsoleKey.Escape);
 		}
 
-		private static byte ReadOddByte(string sPrompt = null, byte byMaxValue = MAX_ASTERISK)
+		private static byte ReadOddByte(string prompt = null, byte maxWaist = MAX_ASTERISK)
 		{
-
-			string sInput = "";
-			byte byWaist = 0;
+			string input = "";
+			byte waist = 0;
 			do
 			{
 				try
 				{
-					Console.Write(sPrompt, byMaxValue);
+					Console.Write(prompt, maxWaist);
 
-					sInput = Console.ReadLine();
-					byWaist = byte.Parse(sInput);
+					input = Console.ReadLine();
+					waist = byte.Parse(input);
 
-					if ((byWaist > byMaxValue) || ((byWaist % 2) != 1))
+					if ((waist > maxWaist) || ((waist % 2) != 1))
 					{
-						viewMessage(string.Format(drawDiamond.rm.GetString("Error_Message"), byMaxValue), true);
+						viewMessage(string.Format(drawDiamond.rm.GetString("Error_Message"), maxWaist), true);
 					}
 				}
 				catch
 				{
-					viewMessage(string.Format(drawDiamond.rm.GetString("Error_Message"), byMaxValue), true);
+					viewMessage(string.Format(drawDiamond.rm.GetString("Error_Message"), maxWaist), true);
 				}
 			}
-			while ((byWaist > byMaxValue) || ((byWaist % 2) != 1));
-			return byWaist;	
-
+			while ((waist > maxWaist) || ((waist % 2) != 1));
+			return waist;	
 		}
 
-		private static void RenderDiamond(byte byMaxCount)
+		private static void RenderDiamond(byte maxWaist)
 		{
-			for (int i = 0; i < (byMaxCount - 1) / 2; i++)	// Upper half, start with one * and finish 
-				RenderRow(byMaxCount, i * 2 + 1);				// with byMaxCount minus one on each side
+			for (int i = 0; i < (maxWaist - 1) / 2; i++)	// Upper half, start with one * and finish 
+				RenderRow(maxWaist, i * 2 + 1);				// with byMaxCount minus one on each side
 
-			RenderRow(byMaxCount, byMaxCount);					// middle row, all *
+			RenderRow(maxWaist, maxWaist);					// middle row, all *
 
-			for (int i = (byMaxCount - 1) / 2; i > 0; i--)	// lower half, start with byMaxCount minus one on each side
-				RenderRow(byMaxCount, i * 2 - 1);				// and finish with just one in the middle
+			for (int i = (maxWaist - 1) / 2; i > 0; i--)	// lower half, start with byMaxCount minus one on each side
+				RenderRow(maxWaist, i * 2 - 1);				// and finish with just one in the middle
 		}
 
-
-
-		private static void RenderRow(int iMaxCount, int iAsteriskCount)
+		private static void RenderRow(int maxWaist, int asteriskCount)
 		{
-			int iIndex = (iMaxCount - iAsteriskCount) / 2;	// How many spaces before first *
+			int iIndex = (maxWaist - asteriskCount) / 2;	// How many spaces before first *
 
 			for (int i = 0; i < iIndex; i++)						// step spaces			
 				Console.Write(" ");
 
-			for (int i = iIndex; i < iIndex + iAsteriskCount; i++)	// draw calculated number of *, given as parameter
+			for (int i = iIndex; i < iIndex + asteriskCount; i++)	// draw calculated number of *, given as parameter
 				Console.Write("*");
 
 			Console.WriteLine();		// line feed
 		}
-
-
-
 	}
 }
